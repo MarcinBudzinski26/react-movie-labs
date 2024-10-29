fd0dcfb74c943e686739f55de584b78904105e4c
marcinbudzinski26 <20081125@mail.wit.ie>
2024-10-29 14:47:58 +0000

    List of movies displayed.

---
+++ movies/src/pages/homePage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import MovieList from "../components/movieList";
+import Grid from "@mui/material/Grid2";
+
+const HomePage = (props) => {
+    const movies = props.movies;
+
+    return(
+        <Grid container>
+            <Grid size={12}>
+                <h1> HomePage </h1>
+
+            </Grid>
+            <Grid container>
+                <MovieList movies={movies}></MovieList>
+            </Grid>
+        </Grid>
+    );
+};
+export default HomePage;
